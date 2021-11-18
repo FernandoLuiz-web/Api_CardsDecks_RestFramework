6d3397721aa294f860621a8b4787887b0706c3d9
FernandoLuiz-web <luizfernandogo.oliveira@outlook.com>
2021-11-18 13:30:12 -0300

    Update 01: Alteração e organização das migrates 0001_initial.py de decks e cards

--- apps/cards/models.py
+++ apps/cards/models.py
@@ -5,13 +5,10 @@ from apps.decks.models import Deck
 class Card(models.Model):
     question = models.CharField(max_length=255)
     answer = models.TextField()
-
     deck = models.ForeignKey(
         Deck,
         on_delete=models.CASCADE,
-        null=True,
-        blank=True
     )
 
     def __str__(self):
-        return f'Your question: {self.question} and the answer is: {self.answer}'
+        return f'The question: {self.question}, the answer: {self.answer}, by deck: f{self.deck}'
--- apps/decks/models.py
+++ apps/decks/models.py
@@ -5,4 +5,4 @@ class Deck(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'Deck: {self.name}'
+        return self.name
