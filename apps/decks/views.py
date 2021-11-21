240d59ce0cdebf6f385f68dc85669382424133d9
Luiz Fernando <luizfernandogo.oliveira@gmail.com>
2021-11-21 11:47:52 -0300

    Novas implementações nas rotas e pesquisas na API

--- apps/decks/views.py
+++ apps/decks/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets, serializers
+from rest_framework import viewsets, serializers, response
 from .models import Deck
+from apps.cards.views import CardSerializer
+from apps.cards.models import Card
 
 
 class DeckSerializer(serializers.ModelSerializer):
@@ -11,3 +13,13 @@ class DeckSerializer(serializers.ModelSerializer):
 class DecksViewSet(viewsets.ModelViewSet):
     serializer_class = DeckSerializer
     queryset = Deck.objects.all()
+
+
+class DeckCardsViewSet(viewsets.ModelViewSet):
+    serializer_class = CardSerializer
+    queryset = Card.objects.all()
+
+    def list(self, request, decks_pk):
+        cards = Card.objects.filter(deck=decks_pk)
+        serializer = self.get_serializer(cards, many=True)
+        return response.Response(serializer.data)
